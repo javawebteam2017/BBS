f551c7d8ae4bc5cbfb3fb6482961f044de527478
miller925 <57781659+miller925@users.noreply.github.com>
2019-12-20 01:17:23 +0800

    Add files via upload

---
+++ PlateController.java
@@ -0,0 +1,11 @@
+package cn.edu.ncu.jiluan.bbs.controller;
+
+import cn.edu.ncu.jiluan.bbs.service.PlateService;
+import cn.edu.ncu.jiluan.bbs.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
