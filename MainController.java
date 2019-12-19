84dd82074d4a8dbc945a41e600d3c5237753a648
miller925 <57781659+miller925@users.noreply.github.com>
2019-12-20 02:06:32 +0800

    Add files via upload

--- MainController.java
+++ MainController.java
@@ -3,13 +3,6 @@ package cn.edu.ncu.jiluan.bbs.controller;
 import cn.edu.ncu.jiluan.bbs.entity.PlateEntity;
 import cn.edu.ncu.jiluan.bbs.entity.PostEntity;
 import cn.edu.ncu.jiluan.bbs.service.PlateService;
-import cn.edu.ncu.jiluan.bbs.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.jws.WebParam;
 import javax.servlet.http.HttpServletRequest;
