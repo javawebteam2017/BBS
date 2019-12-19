a38d3f847e7cfb4a7762a39a239862ed36a1c81c
784582793wgy <57864344+784582793wgy@users.noreply.github.com>
2019-12-20 02:55:23 +0800

    Update PostController.java

--- PostController.java
+++ PostController.java
@@ -1,20 +1,4 @@
-package cn.edu.bbs.controller;
 
-import cn.edu.bbs.entity.PostEntity;
-import cn.edu.bbs.service.PlateService;
-import cn.edu.bbs.service.PostLikedService;
-import cn.edu.bbs.service.PostService;
-import cn.edu.bbs.service.ReplyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.sql.Timestamp;
 
 /**
